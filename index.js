8485d1a7188f1b47bb0d14ba5a8cbdb547bca4d4
Alex <alexflorea123@gmail.com>
2024-03-27 15:27:40 -0400

    TimerCountDownDone

--- index.js
+++ index.js
@@ -119,6 +119,17 @@ function rectangularCollision({
    rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
 }
+let timer = 10;
+function decreaseTimer(){
+  setTimeout(decreaseTimer, 1000)
+if(timer>0){
+  timer--
+  document.querySelector('#timer').innerHTML = timer
+}
+  
+}
+decreaseTimer();
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
