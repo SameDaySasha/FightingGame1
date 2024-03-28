efed526c7b4abb173f505a465c8b1370f88ea4d2
Alex <alexflorea123@gmail.com>
2024-03-28 15:07:44 -0400

    ClassRefactor+BackgroundSprite

--- js/classes.js
+++ js/classes.js
@@ -56,7 +56,7 @@ class Sprite {
       this.position.x += this.velocity.x;
       this.position.y += this.velocity.y;
   
-      if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
         this.velocity.y = 0;
       } else this.velocity.y += gravity;
     }
